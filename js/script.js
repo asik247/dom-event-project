6feac6fb405c12a033bb972b76c85abc15874867
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-15 00:55:44 +0600

    emty code fixed

--- js/script.js
+++ js/script.js
@@ -5,16 +5,21 @@ SecondaryBtn.addEventListener("click", (e) => {
     // Generate phoneNumber and PinNumber;
     const gPhone = 12345678910;
     const gPin = 1234;
-    let phoneNumber = document.getElementById("phoneNumber");
-    let phoneNumberValue = parseInt(phoneNumber.value)
-    // console.log(phoneNumberValue);
-    let pinNumber = document.getElementById("pinNumber");
-    let pinNumberValue = parseInt(pinNumber.value);
+    let phoneNumber = document.getElementById("phoneNumber").value;
+    let pinNumber = document.getElementById("pinNumber").value;
+
+    if (phoneNumber === "" || pinNumber === "") {
+        alert("Please enter phone number and pin");
+        return;
+    }
+    let phoneNumberValue = parseInt(phoneNumber)
+    let pinNumberValue = parseInt(pinNumber);
     // console.log(pinNumberValue);
     // condition code here;
+
     if (gPhone === phoneNumberValue && gPin === pinNumberValue) {
         // console.log("value Matched");
-        window.location.href="/html/next.html"
+        window.location.href = "/html/next.html"
     } else {
         // console.log("invalid cadiential");
         alert("Invalid number or pin")
