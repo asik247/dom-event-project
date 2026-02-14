0ecba68b1b4db0e05eab240bcf72706d7043fe70
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-15 00:32:17 +0600

    js condition code added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 // Login button functionality code start here;
 let SecondaryBtn = document.getElementById("SecondaryBtn");
-SecondaryBtn.addEventListener("click",(e)=>{
+SecondaryBtn.addEventListener("click", (e) => {
     e.preventDefault();
+    // Generate phoneNumber and PinNumber;
+    const gPhone = 1234567891;
+    const gPin = 1234;
     console.log("hi SecondaryBtn clicked");
+    let phoneNumber = document.getElementById("phoneNumber");
+    let phoneNumberValue = parseInt(phoneNumber.value)
+    // console.log(phoneNumberValue);
+    let pinNumber = document.getElementById("pinNumber");
+    let pinNumberValue = parseInt(pinNumber.value);
+    // console.log(pinNumberValue);
+    // condition code here;
+    if(gPhone === phoneNumberValue && gPin === pinNumberValue){
+        console.log("value Matched");
+    }else{
+        console.log("invalid cadiential");
+    }
 })
