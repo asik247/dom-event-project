b5c15c5663edc47104a271bc0e552a6c312b8328
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-15 17:57:39 +0600

    some condition code start now

--- js/next.js
+++ js/next.js
@@ -1,7 +1,33 @@
-console.log("hi iam next page js file");
+// console.log("hi iam next page js file");
 // addMoneyBtn code start here;
 let addMoneyBtn = document.getElementById("addMoneyBtn");
 addMoneyBtn.addEventListener("click",(e)=>{
     e.preventDefault()
-    console.log("Add Money btn connected");
+    // console.log("Add Money btn connected");
+    let selectBank = document.getElementById("selectBank").value;
+    let bankAccountNumber = document.getElementById("bankAccountNumber").value;
+    let addAmount = parseInt(document.getElementById("addAmount").value)
+    let pinNumber = document.getElementById("pinNumber").value;
+    let availableBalance = parseInt(document.getElementById("availableBalance").innerText);
+
+    // bankAccountNumber condiion code start here;
+    if(bankAccountNumber.length < 11){
+        alert("please provide 11 digit account number");
+        return;
+    }else{
+        alert("Add Amount Successfully")
+        // return;
+    }
+
+
+
+
+
+    const totalNewAvailableBalance = addAmount + availableBalance;
+    document.getElementById("availableBalance").innerText = totalNewAvailableBalance;
+
+    // console.log(totalNewAvailableBalance);
+    // console.log(typeof(availableBalance));
+    // console.log(typeof(addAmount));
+    // console.log(selectBank,bankAccountNumber,addAmount,pinNumber);
 })
