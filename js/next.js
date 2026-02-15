0accadc31c438774b54f0dfb9c0e69b8007c2d95
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-15 16:48:46 +0600

    Add Money Btn code start now

--- js/next.js
+++ js/next.js
@@ -1,1 +1,7 @@
 console.log("hi iam next page js file");
+// addMoneyBtn code start here;
+let addMoneyBtn = document.getElementById("addMoneyBtn");
+addMoneyBtn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    console.log("Add Money btn connected");
+})
