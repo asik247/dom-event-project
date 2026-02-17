a442ea7be44458ac81de7e314c8008ca7e8055e6
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-17 16:45:16 +0600

    Toggling code reusable best way

--- js/next.js
+++ js/next.js
@@ -57,7 +57,7 @@ casOutMoneyBtn.addEventListener("click", (e) => {
     let wAmount = getInputFieldValueNumber("wAmount")
     let agentNumber = getInputFieldValue("agentNumber");
     let casPinNumber = getInputFieldValue("casPinNumber")
-    let availableBalance =getInnerText("availableBalance");
+    let availableBalance = getInnerText("availableBalance");
     // Agent number validation code start here;
     if (agentNumber.length < 11) {
         alert("please provide 11 digit agent number");
@@ -90,81 +90,32 @@ casOutMoneyBtn.addEventListener("click", (e) => {
 // btn add toggoling code start here + btnAdd code:-
 let btnAdd = document.getElementById("btnAdd");
 btnAdd.addEventListener("click", (e) => {
-    let transAll = getDisplayNone("transAll");
-    let payBAll = getDisplayNone("payBAll")
-    let getBAll = getDisplayNone("getBAll");
-    let transferAll = getDisplayNone("transferAll");
-    let casAll = getDisplayNone("casAll");
-    let addAll = document.getElementById("addAll").style.display = "block"
+    handleToggling("addAll")
 })
 // btnCas code here now;
 let btnCas = document.getElementById("btnCas");
 btnCas.addEventListener("click", (e) => {
-    let addAll = getDisplayNone("addAll");
-    let transAll = getDisplayNone("transAll");
-    let payBAll = getDisplayNone("payBAll")
-    let getBAll = getDisplayNone("getBAll");
-    let transferAll = getDisplayNone("transferAll");
-    //  console.log(addAll,payBAll,transAll,transferAll,getBAll);
-    let casAll = document.getElementById("casAll").style.display = "block"
+    handleToggling("casAll")
 })
 // btnTransferMoney code here now;
 let btnTransferMoney = document.getElementById("btnTransferMoney");
 btnTransferMoney.addEventListener("click", () => {
-    let addAll = getDisplayNone("addAll");
-    let casAll = getDisplayNone("casAll");
-    let transAll = getDisplayNone("transAll");
-    let payBAll = getDisplayNone("payBAll")
-    let getBAll = getDisplayNone("getBAll");
-    let transferAll = document.getElementById("transferAll").style.display = "block"
+    handleToggling("transferAll")
 })
 // btnGetBonus code start here;
 let btnGetBonus = document.getElementById("btnGetBonus");
 btnGetBonus.addEventListener("click", () => {
-    let addAll = getDisplayNone("addAll");
-    let casAll = getDisplayNone("casAll");
-    let transAll = getDisplayNone("transAll");
-    let transferAll = getDisplayNone("transferAll");
-    let payBAll = getDisplayNone("payBAll")
-    //  console.log(addAll,casAll,transAll,transferAll,payBAll);
-    let getBAll = document.getElementById("getBAll").style.display = "block"
-
+    handleToggling("getBAll")
 })
 // btnPayBill code start here;
 let btnPayBill = document.getElementById("btnPayBill");
 btnPayBill.addEventListener("click", () => {
-    // console.log("btnTransferMoney btn clicked");
-    let addAll = getDisplayNone("addAll");
-
-    let casAll = getDisplayNone("casAll");
-
-    let transAll = getDisplayNone("transAll");
-
-    let transferAll = getDisplayNone("transferAll");
-
-    let getBAll = getDisplayNone("getBAll");
-    // console.log(addAll,casAll,transAll,transferAll);
-    let payBAll = document.getElementById("payBAll").style.display = "block"
-
-
+    handleToggling("payBAll")
 })
 // btnTransactions code start here
 let btnTransactions = document.getElementById("btnTransactions");
 btnTransactions.addEventListener("click", () => {
-    let addAll = getDisplayNone("addAll");
-
-    let payBAll = getDisplayNone("payBAll");
-
-    let casAll = getDisplayNone("casAll");
-
-    let transferAll = getDisplayNone("transferAll");
-
-    let getBAll = getDisplayNone("getBAll");
-
-    // console.log(addAll,payBAll,casAll,transferAll,getBAll);
-
-    let transAll = document.getElementById("transAll").style.display = "block"
-
+    handleToggling("transAll")
 })
 // btnTransactions code end here + toggling code end;
 
@@ -178,14 +129,18 @@ function getInputFieldValue(id) {
 function getInnerText(id) {
     return parseInt(document.getElementById(id).innerText)
 }
-function setInnerText(value){
+function setInnerText(value) {
     document.getElementById("availableBalance").innerText = value;
 }
 
 // Reusable funk code end here;
 
 // Btn Togglie Reusable funk code start here;
-function getDisplayNone(id) {
-    return document.getElementById(id).style.display = "none";
+function handleToggling(id) {
+    const forms = document.getElementsByClassName("form2");
+    for (const form of forms) {
+        form.style.display = "none"
+    }
+    document.getElementById(id).style.display = "block"
 }
 // Btn Togglie Reusable funk code end here;
