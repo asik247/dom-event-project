44d58fd808cca3eade8c23a4eea38a1566ca84ac
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-17 14:42:31 +0600

    Reusable funk build

--- js/next.js
+++ js/next.js
@@ -1,14 +1,20 @@
+// Globaly declare code start here;
 const gPinNumber = "1234"
 let selectBank = document.getElementById("selectBank");
 let browsers = document.getElementById("browsers");
+// Globaly declare code end here;
+
 // addMoneyBtn code start here;
 let addMoneyBtn = document.getElementById("addMoneyBtn");
 addMoneyBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    let bankAccountNumber = document.getElementById("bankAccountNumber").value;
-    let addAmount = parseInt(document.getElementById("addAmount").value)
-    let pinNumber = document.getElementById("pinNumber").value.trim();
-    console.log(typeof (pinNumber));
+
+    let bankAccountNumber = getInputFieldValue("bankAccountNumber");
+   
+    let addAmount = getInputFieldValueNumber("addAmount");
+    
+    let pinNumber = getInputFieldValue("pinNumber");
+ 
     let availableBalance = parseInt(document.getElementById("availableBalance").innerText);
 
     // ---- Bank Validation ----
@@ -44,6 +50,48 @@ addMoneyBtn.addEventListener("click", (e) => {
     const totalNewAvailableBalance = addAmount + availableBalance;
     document.getElementById("availableBalance").innerText = totalNewAvailableBalance;
 })
+// addMoneyBtn code end here;
+
+// Cash Out input field code start here;
+let casOutMoneyBtn = document.getElementById("casOutMoneyBtn");
+const gCashOutPin = "4321"
+casOutMoneyBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let wAmount = getInputFieldValueNumber("wAmount")
+   
+    let agentNumber = getInputFieldValue("agentNumber");
+   
+
+    let casPinNumber = getInputFieldValue("casPinNumber")
+    
+    let availableBalance = parseInt(document.getElementById("availableBalance").innerText);
+
+    // Agent number validation code start here;
+    if (agentNumber.length < 11) {
+        alert("please provide 11 digit agent number");
+        return;
+    }
+    // cash out ammount validiton code start here;
+    if (isNaN(wAmount) || wAmount <= 0) {
+        alert("Enter a valid amount")
+        return;
+    }
+    // pin number condition code start here;
+    if (casPinNumber.length !== 4) {
+        alert("PIN must be 4 digits");
+        return;
+    }
+    if (gCashOutPin !== casPinNumber) {
+        alert("Incorrect pin")
+        return;
+    }
+    else {
+        alert("Withdrawo Amoutn Successfully")
+    }
+    const totalNewAvailableBalance = availableBalance - wAmount;
+    document.getElementById("availableBalance").innerText = totalNewAvailableBalance;
+})
+// Cash Out input field code end here;
 
 // btn add toggoling code start here + btnAdd code:-
 let btnAdd = document.getElementById("btnAdd");
@@ -68,7 +116,6 @@ btnCas.addEventListener("click", (e) => {
 
 
 })
-
 // btnTransferMoney code here now;
 let btnTransferMoney = document.getElementById("btnTransferMoney");
 btnTransferMoney.addEventListener("click", () => {
@@ -108,6 +155,7 @@ btnPayBill.addEventListener("click", () => {
 
 
 })
+// btnTransactions code start here
 let btnTransactions = document.getElementById("btnTransactions");
 btnTransactions.addEventListener("click", () => {
     // console.log("btnTransferMoney btn clicked");
@@ -119,41 +167,13 @@ btnTransactions.addEventListener("click", () => {
     let transAll = document.getElementById("transAll").style.display = "block"
 
 })
+// btnTransactions code end here + toggling code end;
 
-
-// Cash Out input field code start here;
-let casOutMoneyBtn = document.getElementById("casOutMoneyBtn");
-const gCashOutPin = "4321"
-casOutMoneyBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let wAmount = parseInt(document.getElementById("wAmount").value)
-    let agentNumber = document.getElementById("agentNumber").value
-    let casPinNumber = document.getElementById("casPinNumber").value.trim();
-    console.log(agentNumber);
-    let availableBalance = parseInt(document.getElementById("availableBalance").innerText);
-    
-    // Agent number validation code start here;
-    if (agentNumber.length < 11) {
-        alert("please provide 11 digit agent number");
-        return;
-    }
-    // cash out ammount validiton code start here;
-     if (isNaN(wAmount) || wAmount <= 0) {
-        alert("Enter a valid amount")
-        return;
-    }
-    // pin number condition code start here;
-    if (casPinNumber.length !== 4) {
-        alert("PIN must be 4 digits");
-        return;
-    }
-    if (gCashOutPin !== casPinNumber) {
-        alert("Incorrect pin")
-        return;
-    }
-    else {
-        alert("Withdrawo Amoutn Successfully")
-    }
-    const totalNewAvailableBalance = availableBalance - wAmount;
-    document.getElementById("availableBalance").innerText = totalNewAvailableBalance;
-})
+// Reusable funk code start here;
+function getInputFieldValueNumber(id) {
+    return parseInt(document.getElementById(id).value);
+}
+function getInputFieldValue(id) {
+    return document.getElementById(id).value;
+}
+// Reusable funk code end here;
